cb562981e3f8c28c96f7c2a9fe35fe1e4646c91b
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-02-20 16:08:22 -0300

    js basiquinho e botoes

--- js/novidades.js
+++ js/novidades.js
@@ -1,3 +1,17 @@
-function enviar(){
-    alert ("Receita enviada com sucesso!")
+function enviar() {
+    let textoReceita = document.getElementById("receita").value;
+    let textoCusto = document.getElementById("custo").value;
+    let textoPorcoes = document.getElementById("porcoes").value;
+
+    if (textoReceita.trim() === "" || textoCusto.trim() === "" || textoPorcoes.trim() === "") {
+        
+        alert("Preencha todos os campos antes de enviar a receita!");
+        
+    } else {
+        alert("Receita enviada com sucesso!");
+        
+        document.getElementById("receita").value = "";
+        document.getElementById("custo").value = "";
+        document.getElementById("porcoes").value = "";
+    }
 }
