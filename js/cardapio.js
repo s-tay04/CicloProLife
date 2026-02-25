9fb986a702e45d946f8edcdc0457fb068a5d34a0
XaropinhaDoSUS <geovanacmoreira@gmail.com>
2026-02-25 17:16:33 -0300

    modo escuro

--- js/cardapio.js
+++ js/cardapio.js
@@ -86,3 +86,34 @@ campoPesquisa.addEventListener("input", function () {
         }
     });
 });
+
+//modo escuro
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const botaoTema = document.getElementById("toggle-tema");
+    const iconeTema = document.getElementById("icone-tema");
+
+    if (!botaoTema) return;
+
+    function aplicarTema(modo) {
+        if (modo === "dark") {
+            document.body.classList.add("dark");
+            iconeTema.src = "../imagem/lua2.png"; 
+        } else {
+            document.body.classList.remove("dark");
+            iconeTema.src = "../imagem/lua.png"; 
+        }
+    }
+
+    botaoTema.addEventListener("click", function () {
+        const novoModo = document.body.classList.contains("dark") ? "light" : "dark";
+        localStorage.setItem("tema", novoModo);
+        aplicarTema(novoModo);
+    });
+
+    // mantém salvo ao atualizar
+    const temaSalvo = localStorage.getItem("tema") || "light";
+    aplicarTema(temaSalvo);
+
+});
