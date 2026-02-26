9ee0d7eae87ea4ffb1d7670d1c20983c6f58fc5f
XaropinhaDoSUS <geovanacmoreira@gmail.com>
2026-02-26 15:07:57 -0300

    modo escuro do editar perfil está pronto

--- js/perfil.js
+++ js/perfil.js
@@ -56,3 +56,34 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+//modo escuro
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const botaoTema = document.getElementById("toggle-tema");
+    const iconeTema = document.getElementById("icone-tema");
+
+    if (!botaoTema) return;
+
+    function aplicarTema(modo) {
+        if (modo === "dark") {
+            document.body.classList.add("dark");
+            iconeTema.src = "../imagem/lua2.png"; 
+        } else {
+            document.body.classList.remove("dark");
+            iconeTema.src = "../imagem/lua.png"; 
+        }
+    }
+
+    botaoTema.addEventListener("click", function () {
+        const novoModo = document.body.classList.contains("dark") ? "light" : "dark";
+        localStorage.setItem("tema", novoModo);
+        aplicarTema(novoModo);
+    });
+
+    // mantém salvo 
+    const temaSalvo = localStorage.getItem("tema") || "light";
+    aplicarTema(temaSalvo);
+
+});
