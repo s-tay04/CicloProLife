e44a9578c98d15d17591e0d48fd1fdc90c61cc8d
XaropinhaDoSUS <geovanacmoreira@gmail.com>
2026-02-27 17:08:02 -0300

    todos os escuros estão prontos

--- js/receitas.js
+++ js/receitas.js
@@ -1,3 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const botaoTema = document.getElementById("toggle-tema");
+    const iconeTema = document.getElementById("icone-tema");
+
+    if (!botaoTema) return;
+
+    function aplicarTema(modo) {
+        if (modo === "dark") {
+            document.body.classList.add("dark");
+            iconeTema.src = "../imagem/lua2.png"; 
+        } else {
+            document.body.classList.remove("dark");
+            iconeTema.src = "../imagem/lua.png"; 
+        }
+    }
+
+    botaoTema.addEventListener("click", function () {
+        const novoModo = document.body.classList.contains("dark") ? "light" : "dark";
+        localStorage.setItem("tema", novoModo);
+        aplicarTema(novoModo);
+    });
+
+
+    const temaSalvo = localStorage.getItem("tema") || "light";
+    aplicarTema(temaSalvo);
+
+});
+
 const bancoDeReceitas = {
     'empada': {
         titulo: 'Empada de Frango',
@@ -165,3 +194,34 @@ if (bancoDeReceitas[idReceita]) {
     tituloElemento.innerText = "Receita não encontrada";
     listaIngredientes.innerHTML = "";
 }
+
+//modo escuro
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const botaoTema = document.getElementById("toggle-tema");
+    const iconeTema = document.getElementById("icone-tema");
+
+    if (!botaoTema) return;
+
+    function aplicarTema(modo) {
+        if (modo === "dark") {
+            document.body.classList.add("dark");
+            iconeTema.src = "../imagem/lua2.png"; 
+        } else {
+            document.body.classList.remove("dark");
+            iconeTema.src = "../imagem/lua.png"; 
+        }
+    }
+
+    botaoTema.addEventListener("click", function () {
+        const novoModo = document.body.classList.contains("dark") ? "light" : "dark";
+        localStorage.setItem("tema", novoModo);
+        aplicarTema(novoModo);
+    });
+
+
+    const temaSalvo = localStorage.getItem("tema") || "light";
+    aplicarTema(temaSalvo);
+
+});
